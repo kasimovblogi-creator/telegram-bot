be4d4b00c7d863f51ad6e7869bcffc073c55e88e
kasimovblogi <kasimovblogi@gmail.com>
2026-04-24 00:05:29 +0300

    add postgres dependency

--- db.py
+++ db.py
@@ -1,36 +1,47 @@
-﻿import sqlite3
+﻿import os
+import psycopg2
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+def get_conn():
+    return psycopg2.connect(DATABASE_URL, sslmode="require")
+
 
 def init_db():
-    conn = sqlite3.connect("users.db")
-    cursor = conn.cursor()
+    conn = get_conn()
+    cur = conn.cursor()
 
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS users (
-        user_id INTEGER PRIMARY KEY
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS saudiasaribot (
+        user_id BIGINT PRIMARY KEY
     )
     """)
 
     conn.commit()
     conn.close()
 
-def add_user(user_id):
-    conn = sqlite3.connect("users.db")
-    cursor = conn.cursor()
 
-    cursor.execute("""
-    INSERT OR IGNORE INTO users (user_id)
-    VALUES (?)
+def add_user(user_id):
+    conn = get_conn()
+    cur = conn.cursor()
+
+    cur.execute("""
+    INSERT INTO saudiasaribot (user_id)
+    VALUES (%s)
+    ON CONFLICT (user_id) DO NOTHING
     """, (user_id,))
 
     conn.commit()
     conn.close()
 
-def get_user_count():
-    conn = sqlite3.connect("users.db")
-    cursor = conn.cursor()
 
-    cursor.execute("SELECT COUNT(*) FROM users")
-    count = cursor.fetchone()[0]
+def get_user_count():
+    conn = get_conn()
+    cur = conn.cursor()
+
+    cur.execute("SELECT COUNT(*) FROM saudiasaribot")
+    count = cur.fetchone()[0]
 
     conn.close()
     return count
